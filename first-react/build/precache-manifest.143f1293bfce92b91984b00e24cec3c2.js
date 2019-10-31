d3e9c150818d09ab5abdf9172f7b64ecd09ae40c
guochaonan <15534418841@163.com>
2019-10-31 09:03:26 +0800

    vscode操作

---
+++ first-react/build/precache-manifest.143f1293bfce92b91984b00e24cec3c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942fc8179a666d870abfbe942596ff32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f366cb85c92c50290031",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "218974ba79e618488980",
+    "url": "./static/js/2.708c725f.chunk.js"
+  },
+  {
+    "revision": "f366cb85c92c50290031",
+    "url": "./static/js/main.ac4cb7e2.chunk.js"
+  },
+  {
+    "revision": "a0ae24648a89daedf176",
+    "url": "./static/js/runtime-main.71f18b76.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
